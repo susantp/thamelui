d68c743f88bfd9d4128308da5c5cce6da2692a80
susant <gracysusant@gmail.com>
2022-05-27 21:00:39 +0545

    state management initiated

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Content from '../components/common/Content'
 import Footer from '../components/common/Footer'
 import Navbar from '../components/common/Navbar'
 import Sidebar from '../components/common/Sidebar'
 
 export default function Layout({ children }) {
-  const [sideBarWidth, setSideBarWidth] = useState()
-  const getViewPort = () => {
-    console.log('port')
-  }
-  const handleSideBarWidth = (width) => {
-    setSideBarWidth(width)
-  }
- const getSidebarWidth = () => {
-   return sideBarWidth
- }
   return (
     <>
       <Navbar></Navbar>
-      <Sidebar doUpdateSideBarWidth={handleSideBarWidth}></Sidebar>
+      <Sidebar></Sidebar>
       <Content>
         {children}
       </Content>
