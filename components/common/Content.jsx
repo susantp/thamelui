dbefdc5537dd3d715d92e562ee28a2b392a1bca7
Susant Paudel <gracysusant@gmail.com>
2022-05-31 16:16:21 +0545

    code refactor and sidebar state managed

--- components/common/Content.jsx
+++ components/common/Content.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { useRecoilValue } from 'recoil'
+import { sidebarState } from '../../store/atoms'
 
-export default function Content({ children }) {
-  return <div className="container ml-64 mt-16  ">{children}</div>;
+export default function Content ({ children }) {
+  const { open } = useRecoilValue(sidebarState)
+  const contentPadding = open ? 'container pl-64 pt-16' : 'container pl-24 pt-16'
+
+  return <div className={contentPadding}>{children}</div>
 }
--- components/common/Sidebar.jsx
+++ components/common/Sidebar.jsx
@@ -1,122 +1,84 @@
-import React, { useState } from "react";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
-import CategoryIcon from "@mui/icons-material/Category";
-import ReceiptIcon from "@mui/icons-material/Receipt";
-import LinkIcon from "@mui/icons-material/Link";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { useRecoilState } from "recoil";
-import { themeState } from "../../store/atoms";
-import Link from "next/link";
+import React, { useRef, useState } from 'react'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
+import CategoryIcon from '@mui/icons-material/Category'
+import ReceiptIcon from '@mui/icons-material/Receipt'
+import LinkIcon from '@mui/icons-material/Link'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { useRecoilState } from 'recoil'
+import { sidebarState } from '../../store/atoms'
+import Link from 'next/link'
 
-export default function Sidebar({ doUpdateSideBarWidth }) {
-  const [isOpen, setisOpen] = useState(true);
+export default function Sidebar () {
+  const [isOpen, setIsOpen] = useState(true)
+  const [{ open }, setSidebar] = useRecoilState(sidebarState)
 
-  const handleClick = () => {
-    setisOpen(!isOpen);
-  };
+  const mainDivRef = useRef(null)
+  const sidebarMenuList = [
+    { name: 'Dashboard', link: '/', icon: <DashboardIcon/> },
+    { name: 'Products', link: '/products', icon: <CategoryIcon/> },
+    { name: 'Customers', link: '/customers', icon: <PersonOutlineIcon/> },
+    { name: 'Subscriptions', link: '/subscriptions', icon: <MonetizationOnIcon/> },
+    { name: 'Invoices', link: '/invoices', icon: <ReceiptIcon/> },
+    { name: 'Plans', link: '/plans', icon: <ReceiptIcon/> },
+    { name: 'Inventories', link: '/inventories', icon: <ReceiptIcon/> },
+    { name: 'Taxes', link: '/taxes', icon: <LinkIcon/> }
+  ]
+
+  /************************** conditional styles start ***********************/
   const customStyle = isOpen
-    ? "flex flex-col  w-60 fixed top-16 bg-gray-200  border-gray-300 left-0 h-screen"
-    : "flex flex-col  w-[78px] fixed top-16 bg-gray-200 	border-gray-300	 left-0 h-screen transition ease-in-out delay-150 ";
+    ? 'flex flex-col  w-60 fixed top-16 bg-gray-200  border-gray-300 left-0 h-screen overflow-auto'
+    : 'flex flex-col  w-[78px] fixed top-16 bg-gray-200 	border-gray-300	 left-0 h-screen transition ease-in-out delay-150 overflow-auto'
 
-  const fontDisplay = isOpen ? "ml-3 text-xs" : "hidden";
+  const fontDisplay = isOpen ? 'ml-3 text-xs' : 'hidden'
 
   const toggleBtnWidth = isOpen
-    ? " flex justify-center pt-2 text-white border cursor-pointer  border-slate-700 fixed bottom-0 h-9 bg-slate-400 w-60"
-    : " flex justify-center pt-2 text-white border cursor-pointer  border-slate-700 fixed bottom-0 h-9 bg-slate-400 w-[78px]";
+    ? ' flex justify-center pt-2 text-white border cursor-pointer  border-slate-700 fixed bottom-0 h-9 bg-slate-400 w-60'
+    : ' flex justify-center pt-2 text-white border cursor-pointer  border-slate-700 fixed bottom-0 h-9 bg-slate-400 w-[78px]'
 
-    const linkDivStyle = "cursor-pointer hover:bg-blue-400 hover:block w-full p-4 "
+  /************************** conditional styles end ***********************/
+  const linkDivStyle = 'cursor-pointer hover:bg-blue-400 hover:block w-full p-4 '
+
+  const SidebarMenuComponent = ({ sidebar }) => {
+    return (
+      <Link href={sidebar.link}>
+        <div className={linkDivStyle}>
+            <span>
+              {sidebar.icon}
+            </span>
+          <span className={fontDisplay}> {sidebar.name} </span>
+        </div>
+      </Link>
+    )
+  }
+
+  const handleClick = () => {
+    setSidebar({open:!isOpen})
+    setIsOpen(!isOpen)
+  }
+
+
   return (
     <>
-      <div className={customStyle}>
-        <Link href="/">
-          <div className={linkDivStyle}>
-            <span>
-              <DashboardIcon />
-            </span>
-            <span className={fontDisplay}> Dashboard </span>
-          </div>
-        </Link>
-        <Link href="/customers">
-          <div className={linkDivStyle}>
-            <span>
-              <PersonOutlineIcon />
-            </span>
-            <span className={fontDisplay}> Customers </span>
-          </div>
-        </Link>
-
-        <Link href="/products">
-          <div className={linkDivStyle}>
-            <span>
-              <CategoryIcon />
-            </span>
-            <span className={fontDisplay}> Products </span>
-          </div>
-        </Link>
-        <Link href="/plans">
-          <div className={linkDivStyle}>
-            <span>
-              <CategoryIcon />
-            </span>
-            <span className={fontDisplay}> Plans </span>
-          </div>
-        </Link>
-
-        <Link href="/payments">
-          <div className={linkDivStyle}>
-            <span>
-              <CategoryIcon />
-            </span>
-            <span className={fontDisplay}> Payments </span>
-          </div>
-        </Link>
-
-        <Link href="/subscriptions">
-          <div className={linkDivStyle}>
-            <span>
-              <MonetizationOnIcon />
-            </span>
-            <span className={fontDisplay}> Subscriptions </span>
-          </div>
-        </Link>
-        <Link href="/invoices">
-          <div className={linkDivStyle}>
-            <span>
-              <ReceiptIcon />
-            </span>
-            <span className={fontDisplay}> Invoices </span>
-          </div>
-        </Link>
-        <Link href="/inventories">
-          <div className={linkDivStyle}>
-            <span>
-              <ReceiptIcon />
-            </span>
-            <span className={fontDisplay}> Inventories </span>
-          </div>
-        </Link>
-        <Link href="/taxes">
-          <div className={linkDivStyle}>
-            <span>
-              <LinkIcon />
-            </span>
-            <span className={fontDisplay}> Taxes </span>
-          </div>
-        </Link>
+      <div className={customStyle} ref={mainDivRef}>
+        {
+          sidebarMenuList.map((sidebar, key) => {
+            return <SidebarMenuComponent key={key} sidebar={sidebar}/>
+          })
+        }
       </div>
 
       {isOpen ? (
         <div onClick={handleClick} className={toggleBtnWidth}>
-          <ArrowBackIcon />
+          <ArrowBackIcon/>
         </div>
       ) : (
         <div onClick={handleClick} className={toggleBtnWidth}>
-          <ArrowForwardIcon />
+          <ArrowForwardIcon/>
         </div>
       )}
     </>
-  );
+  )
 }
