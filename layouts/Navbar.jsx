7cd88b68a6cfc811d78ebdec5d0336a7cb0715fb
rishav <rishav.pokharel@newitventure.com>
2022-06-10 12:06:57 +0545

    dark mode wip

--- layouts/Navbar.jsx
+++ layouts/Navbar.jsx
@@ -25,39 +25,40 @@ export default function Navbar() {
   //   ? "flex flex-row basis-1/2 gap-x-8 items-center text-sm cursor-pointer"
   //   : "flex flex-row basis-1/2 gap-x-8 items-center text-sm cursor-pointer";
   return (
-    <nav className="flex flex-row justify-between p-4 h-20 drop-shadow-md sticky top-0 bg-white mb-4">
-        <div className="gap-x-8 items-center text-sm cursor-pointer">
-          <SearchSharpIcon className="w-5 h-5 absolute ml-3 mt-4 pointer-events-none" />
-          <input
-            type="text"
-            name="search"
-            placeholder="Search or type keyword"
-            autoComplete="off"
-            aria-label="Search..."
-            className="w-full pr-3 pl-10 py-1 font-normal placeholder-[#b2b2b4] text-[#B2b2b4]"
-          />
+    <nav className="flex flex-row justify-between p-4 h-20 drop-shadow-md sticky top-0  bg-white dark:bg-black mb-4">
+      <div className="gap-x-8 items-center text-sm cursor-pointer">
+        <SearchSharpIcon className="w-5 h-5 absolute ml-3 mt-4 pointer-events-none dark:text-white " />
+        <input
+          type="text"
+          name="search"
+          placeholder="Search or type keyword"
+          autoComplete="off"
+          aria-label="Search..."
+          className="w-full pr-3 pl-10 py-4 font-normal dark:bg-black placeholder-[#b2b2b4] text-[#B2b2b4]"
+        />
+      </div>
+
+      <div
+        className={`flex flex-row gap-x-8 items-center text-sm cursor-pointer`}
+      >
+        <div className="flex justify-evenly align-middle bg-[#e4f2fd] p-2 rounded-full w-24">
+          <span className="bg-white rounded-full">
+            <LightModeOutlinedIcon />
+          </span>
+          <span>
+            <NightlightOutlinedIcon />
+          </span>
         </div>
 
-        <div className={`flex flex-row gap-x-8 items-center text-sm cursor-pointer`}>
-          <div className="flex justify-evenly align-middle bg-[#e4f2fd] p-2 rounded-full w-24">
-            <span className="bg-white rounded-full">
-              <LightModeOutlinedIcon />
-            </span>
-            <span>
-              <NightlightOutlinedIcon />
-            </span>
-          </div>
-
-          <div>
-            {/* <NotificationsIcon className="cursor-pointer" onClick={onDisplay} /> */}
-            <img className="w-8" src="./notification.png" alt="" />
-          </div>
-          <div>
-            {/* <p className="font-bold text-[16px]">Zara Larsson</p> */}
-            <img src="./profileimage.png" alt="" />
-          </div>
+        <div>
+          {/* <NotificationsIcon className="cursor-pointer" onClick={onDisplay} /> */}
+          <img className="w-8" src="./notification.png" alt="" />
         </div>
-      </nav>
-    </>
+        <div>
+          {/* <p className="font-bold text-[16px]">Zara Larsson</p> */}
+          <img src="./profileimage.png" alt="" />
+        </div>
+      </div>
+    </nav>
   );
 }
