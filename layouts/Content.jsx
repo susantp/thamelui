c9fdb35364e62c673d718d81b84d3da7bbe1e7d3
rishav <rishav.pokharel@newitventure.com>
2022-06-10 17:44:43 +0545

    designed fixed

--- layouts/Content.jsx
+++ layouts/Content.jsx
@@ -6,8 +6,8 @@ import { sidebarState } from "../store/atoms";
 export default function Content({ children }) {
   const { open } = useRecoilValue(sidebarState);
   const contentPadding = open
-    ? "container ml-[17rem] pt-6 transition-all  duration-300 ease-in-out  "
-    : "container ml-48 pt-6 transition-all  duration-300 ease-in-out ";
+    ? " mr-8 md:ml-[17rem] xl:ml-[17rem]  pt-6 transition-all  duration-300 ease-in-out  "
+    : "ml-28 mr-6 pt-6 transition-all  duration-300 ease-in-out ";
 
   return <div className={contentPadding}>{children}</div>;
 }
