0bac2a66317e5d7a8364979610215e780a16be4e
Susant Paudel <gracysusant@gmail.com>
2022-06-16 02:06:34 +0545

    some change

--- layouts/Sidebar.jsx
+++ layouts/Sidebar.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
-import useStyles from '../hooks/dashboard/useStyles'
-import useLocalData from '../hooks/dashboard/useData'
+import useStyles from '../hooks/sidebar/useStyles'
+import useLocalData from '../hooks/sidebar/useData'
 
 export default function Sidebar () {
-
-  const { fontDisplay,customStyle, linkDivStyle, imageWidth } = useStyles()
+  const { fontDisplay, customStyle, linkDivStyle, imageWidth } = useStyles()
   const { sidebarMenuList } = useLocalData()
-  // const customStyle = open ? 'flex flex-col w-60 fixed pt-32 bg-white border-gray-300 left-0 h-screen overflow-auto drop-shadow-xl z-40 dark:bg-black dark:text-white  transition-all  duration-300 ease-in-out' : 'flex flex-col  w-24 fixed pt-32 bg-white border-gray-300	left-0 h-screen overflow-auto drop-shadow-xl  dark:bg-black dark:text-white transition-all  duration-300 ease-in-out'
-
   return (
-      <div className={customStyle}>
-        {sidebarMenuList.map((sidebar, index) =>
-          (
-            <Link key={index} href={sidebar.link}>
-              <div className={linkDivStyle}>
-                <img className={imageWidth} src={sidebar.icon} alt={sidebar.icon}/>
-                <span className={fontDisplay}> {sidebar.name} </span>
-              </div>
-            </Link>
-          )
-        )}
-      </div>
+    <div className={customStyle}>
+      {sidebarMenuList.map((sidebar, index) =>
+        (
+          <Link key={index} href={sidebar.link}>
+            <div className={linkDivStyle}>
+              <img className={imageWidth} src={sidebar.icon} alt={sidebar.icon}/>
+              <div className={fontDisplay}> {sidebar.name} </div>
+            </div>
+          </Link>
+        )
+      )}
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,46 @@
-import Layout from "../layouts/Layout";
-import PlanSummary from "../components/dashboard/PlanSummary";
-import SubscriptionSummary from "../components/dashboard/SubscriptionSummary";
-import Statistic from "../components/dashboard/Statistic";
-import SignupStatics from "../components/dashboard/SignupStatics";
-import Footer from "../layouts/Footer";
+import Layout from '../layouts/Layout'
+import PlanSummary from '../components/dashboard/PlanSummary'
+import SubscriptionSummary from '../components/dashboard/SubscriptionSummary'
+import Statistic from '../components/dashboard/Statistic'
+import SignupStatics from '../components/dashboard/SignupStatics'
+import Footer from '../layouts/Footer'
 
-import axios from "axios";
-export default function Index({ data }) {
+import axios from 'axios'
+
+export default function Index ({ data }) {
   return (
     <Layout>
-      <div className="flex flex-row justify-between gap-6">
+      <div className="flex flex-col xl:flex-row justify-between gap-6">
         <div className="flex flex-col gap-8 basis-5/6 ">
           <div>
-            <PlanSummary plans={data.planSummary} />
+            <PlanSummary plans={data.planSummary}/>
           </div>
 
           <div>
-            <SubscriptionSummary subscriptions={data.subscriptionSummary} />
+            <SubscriptionSummary subscriptions={data.subscriptionSummary}/>
           </div>
 
           <div>
-            <Footer />
+            <Footer/>
           </div>
         </div>
         <div className="  flex flex-col gap-8 basis-2/6">
-          <Statistic statistics={data.statistics} />
-          <SignupStatics signupstatics={data.signups} />
+          <Statistic statistics={data.statistics}/>
+          <SignupStatics signupstatics={data.signups}/>
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
 export const getServerSideProps = async (Context) => {
-  let data = null;
-  let errors = null;
+  let data = null
+  let errors = null
   try {
-    const res = await axios.get(`http://localhost:3000/api/dashboard`);
-    data = res.data;
+    const res = await axios.get(`http://localhost:3000/api/dashboard`)
+    data = res.data
   } catch (err) {
-    errors = err.message;
+    errors = err.message
     // console.log(errors);
   }
 
@@ -48,5 +49,5 @@ export const getServerSideProps = async (Context) => {
       data,
       errors,
     },
-  };
-};
+  }
+}
