d22661af68791b96b2199ff845f104b9f3b6bed2
Susant Paudel <techbizznepal@gmail.com>
2022-06-30 00:07:52 +0545

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,19 +18,22 @@ export default function Index(callback, deps) {
     console.log('password error - ', passwordError)
     console.groupEnd()
     return (
-        <div className={`flex items-center bg-gray-200  flex-col w-full gap-y-3 h-screen`}>
+        <div className={`flex items-center bg-white flex-col w-full gap-y-3 h-screen`}>
             {/*form div*/}
+
             <form
+
                 onSubmit={handleSubmit}
                 ref={loginFormRef}
                 id={`loginContentWrapper`}
-                className={`flex items-center bg-gradient-to-tr from-purple-50 to-purple-900 p-6 flex-col h-screen basis-11/12 w-full`}>
-
+                className={`flex items-center p-6 flex-col h-screen basis-11/12 w-full`}>
                 {/*email formGroup*/}
                 <div id={`loginFormGroup`}
-                     className={`flex m-auto items-center flex-col gap-y-6 w-full xl:w-1/5 md:w-3/5 sm:w-4/5   bg-white bg-opacity-30 p-8 rounded-md shadow-2xl`}>
+                     className={`flex m-auto items-center bg-gradient-to-tr from-purple-200 to-blue-200 flex-col gap-y-10 w-full xl:w-1/5 md:w-3/5 sm:w-4/5 bg-opacity-30 p-8 rounded-md shadow-2xl`}>
                     <div>
-                        <h2 className={`text-2xl font-bold text-slate-700`}>Thamelmart</h2>
+                        <h2 className={`text-2xl font-bold text-white shadow-xl`}>
+                            <img src={`https://picsum.photos/250/80`} alt={`thamelmart logo`} />
+                        </h2>
                     </div>
                     <div className={`flex flex-col w-64 gap-y-2 sm:w-4/5`} id={`loginEmail`}>
                         <input id={`emailInput`}
@@ -66,7 +69,7 @@ export default function Index(callback, deps) {
             </form>
 
             <div id={`footerWrapper`} className={`h-screen m-auto items-center basis-1/12`}>
-                <div className={`text-6xl text-slate-500`} id={`footerContent`}>Footer</div>
+                <div className={`text-3xl text-slate-500`} id={`footerContent`}>Thamelmart Inc.</div>
             </div>
         </div>
     )
